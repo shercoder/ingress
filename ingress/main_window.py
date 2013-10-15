91ca7433774094a1a4239a63aefc98aae6eea055
Pardeep Singh <singh.pardeep6605@gmail.com>
2013-10-14 17:38:07 -0700

    Fix row expanding bug.
    Fix row expanding bug where the files were generated twice upon expanding.
    Fix bug related to notebook where pages get added and deleted upon selection.

--- ingress/main_window.py
+++ ingress/main_window.py
@@ -20,7 +20,6 @@ class IngressMainWindow(Gtk.Window):
 
         #window paned
         self.add_paned()
-        self.display_selected_file_info()
 
         # set css provider
         self._style_provider = Gtk.CssProvider()
@@ -53,67 +52,48 @@ class IngressMainWindow(Gtk.Window):
         self._store = IngressTreeStore()
         self._treeview = IngressTreeView(self._store)
         self._treeview.set_enable_tree_lines(True)
+        self._treeview.get_selection().connect("changed", self.on_tree_selection_changed)
 
     def create_notebook(self):
         self._notebook = Gtk.Notebook()
         self._notebook.set_tab_pos(Gtk.PositionType.TOP)
-        self.create_general_tab()
-        self.create_permissions_tab()
         return self._notebook
-
-    def display_selected_file_info(self):
-        selection = self._treeview.get_selection()
-        selection.connect("changed", self.on_tree_selection_changed)
 
     def on_tree_selection_changed(self, selection):
         model, treeiter = selection.get_selected()
         if treeiter != None:
-            self.update_general_tab(model[treeiter][1])
-            self.update_permissions_tab(model[treeiter][1])
+            # remove all pages
+            for page in self._notebook.get_children():
+                self._notebook.remove(page)
 
-    def update_general_tab(self, filepath):
-        grid = self._notebook.get_nth_page(0)
+            self.create_general_tab(model[treeiter][1])
+            self.create_permissions_tab(model[treeiter][1])
+            self._notebook.show_all()
+
+    def create_general_tab(self, filepath):
+        # General Page
+        grid = Gtk.Grid(row_spacing=2, column_spacing=2, column_homogeneous=True)
+        grid.set_name('GeneralTab')
+        self._notebook.append_page(grid, Gtk.Label(label="General"))
 
         filestat = Util.get_file_stat(filepath)
-        grid.get_children()[0].set_text(time.ctime(filestat.st_atime))
-        grid.get_children()[2].set_text(time.ctime(filestat.st_mtime))
-        grid.get_children()[4].set_text(os.path.dirname(filepath))
 
         # if dir then Ask user to calculate
         filesize = str(Util.get_filesize_format(filestat.st_size))
         if os.path.isdir(filepath):
             filesize = "Calculate"
 
-        grid.get_children()[6].set_label(filesize)
-        grid.get_children()[8].set_text(os.path.basename(filepath))
-
-    def update_permissions_tab(self, filepath):
-        grid = self._notebook.get_nth_page(1)
-        filestat = Util.get_file_stat(filepath)
-        owner = Util.get_usrname_from_uid(filestat.st_uid)
-        group = Util.get_grpname_from_gid(filestat.st_gid)
-
-        grid.get_children()[0].set_text(group.gr_name)
-        grid.get_children()[2].set_text(owner.pw_name)
-        # self._notebook.next_page()
-
-    def create_general_tab(self):
-        # General Page
-        grid = Gtk.Grid(row_spacing=2, column_spacing=2, column_homogeneous=True)
-        grid.set_name('GeneralTab')
-        self._notebook.append_page(grid, Gtk.Label(label="General"))
-
         # Create labels for general tab
         filename_label = Util.create_label("Name:")
-        filename = Util.create_label("shercoder")
+        filename = Util.create_label(os.path.basename(filepath))
         filesize_label = Util.create_label("Filesize:")
-        filesize = Gtk.Button(label="Calculate")
+        filesize = Gtk.Button(label=filesize)
         location_label = Util.create_label("Location:")
-        location = Util.create_label("location goes here")
+        location = Util.create_label(os.path.dirname(filepath))
         last_modified_label = Util.create_label("Last Modified:")
-        last_modified = Util.create_label("last modified goes here")
+        last_modified = Util.create_label(time.ctime(filestat.st_mtime))
         last_access_label = Util.create_label("Last Access:")
-        last_access = Util.create_label("last access goes here")
+        last_access = Util.create_label(time.ctime(filestat.st_atime))
 
         filesize.connect("clicked", self.on_clicked_filesize_button)
 
@@ -129,27 +109,32 @@ class IngressMainWindow(Gtk.Window):
         grid.attach(last_access_label, 0, 4, 1, 1)
         grid.attach(last_access, 1, 4, 1, 1)
 
-    def create_permissions_tab(self):
+    def create_permissions_tab(self, filepath):
         # Permissions Page
         grid = Gtk.Grid(row_spacing=2, column_spacing=2, column_homogeneous=True)
         grid.set_name('PermissionsGrid')
         self._notebook.append_page(grid, Gtk.Label(label="Permissions"))
 
+        # stat the file
+        filestat = Util.get_file_stat(filepath)
+        owner_name = Util.get_usrname_from_uid(filestat.st_uid).pw_name
+        group_name = Util.get_grpname_from_gid(filestat.st_gid).gr_name
+
         # Owner
+        owner_box = Gtk.Box.new(Gtk.Orientation.HORIZONTAL, 5)
         owner_label = Util.create_label("Owner:")
-        owner = Util.create_label("Owner??")
-        grid.add(owner_label)
-        grid.attach(owner, 1, 0, 1, 1)
+        owner = Util.create_label(owner_name)
+        owner_box.pack_start(owner_label, False, True, 0)
+        owner_box.pack_start(owner, False, True, 20)
+        grid.add(owner_box)
 
         # Group
         grp_box = Gtk.Box.new(Gtk.Orientation.HORIZONTAL, 5)
         group_label = Util.create_label("Group:")
-        group = Util.create_label("Group??")
+        group = Util.create_label(group_name)
         grp_box.pack_start(group_label, False, True, 0)
-        grp_box.pack_start(group, False, True, 20)
+        grp_box.pack_start(group, False, True, 22)
         grid.attach(grp_box, 0, 1, 2, 1)
-        # grid.attach(group_label, 0, 1, 1, 1)
-        # grid.attach(group, 1, 1, 1, 1)
 
     def on_clicked_filesize_button(self, button):
         (model, sel_iter) = self._treeview.get_selection().get_selected()

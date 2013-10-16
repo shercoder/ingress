ff9008bc816dab676793a4653c4493d3b5a3a8f3
Pardeep Singh <singh.pardeep6605@gmail.com>
2013-10-16 01:00:27 -0700

    Squashed commit of the following:
    commit 824ba3ee56a4c178dbe76c02c5570203398aae2c
    Author: Pardeep Singh <singh.pardeep6605@gmail.com>
    Date:   Wed Oct 16 00:57:25 2013 -0700
    
        Add git basic information
    
        Create Repository class with bunch of useful methods.
        Fix minor formatting things for the labels

---
+++ ingress/git_repo.py
@@ -0,0 +1,57 @@
+from pygit2 import Repository as _Repository
+from pygit2 import *
+from gi.repository import Gtk
+
+class Repository(_Repository):
+    def __init__(self, repopath):
+        super().__init__(repopath)
+
+    def get_cur_branch(self):
+        pass
+
+    def branches(self):
+        return self.listall_branches()
+
+    def branch_list_store(self):
+        list_store = Gtk.ListStore(str)
+        for branch in self.branches():
+            list_store.append([branch])
+        return list_store
+
+    def get_status_current(self):
+        status = self.status()
+        files = list()
+        for filepath, flags in status.items():
+            if flags == GIT_STATUS_CURRENT:
+                files.append(filepath)
+
+    def get_status_ignored(self):
+        status = self.status()
+        files = list()
+        for filepath, flags in status.items():
+            if flags == GIT_STATUS_IGNORED:
+                files.append(filepath)
+
+    def get_status_wt(self):
+        status = self.status()
+        files = dict()
+        for filepath, flags in status.items():
+            if flags == GIT_STATUS_WT_DELETED or \
+                flags == GIT_STATUS_WT_MODIFIED or \
+                flags == GIT_STATUS_WT_NEW:
+                files[filepath] = flags
+
+    def get_status_index(self):
+        status = self.status()
+        files = dict()
+        for filepath, flags in status.items():
+            if flags == GIT_STATUS_INDEX_DELETED or \
+                flags == GIT_STATUS_INDEX_MODIFIED or \
+                flags == GIT_STATUS_INDEX_NEW:
+                files[filepath] = flags
+
+    def commit_count(self):
+        counter = 0
+        for commit in self.walk(self.head.target, GIT_SORT_TOPOLOGICAL):
+            counter +=1
+        return counter
